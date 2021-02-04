5c2abf2213a955a2d5bec9028a6ba58f7ae0b6ad
SariMelina <mmkings_beba@hotmail.com>
2021-02-04 15:28:50 -0600

    Users component created

---
+++ src/components/Users.component.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <p>Lista de usuarios</p>
+        <ul class="list-group">
+            <li class="list-group-item"
+            v-for="(user, index) in users" :key="index">
+                {{ user.name }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+      return {
+          users : {}
+      } 
+    },
+   created () {
+       this.$http.get('https://jsonplaceholder.typicode.com/users')
+       .then(resp => {
+           return resp.json()
+       }).then(users => {
+           this.users = users
+       })
+   }
+}
+</script>
