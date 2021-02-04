1047606d64c0c56867b901e8ed66ce3fdfdcacf0
SariMelina <mmkings_beba@hotmail.com>
2021-02-04 15:29:18 -0600

    New route added

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -1,5 +1,7 @@
 import HomeComponent from '../components/Home.component.vue'
+import UsersComponent from '../components/Users.component.vue'
 
 export const rutas = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: '/users', component: UsersComponent }
 ]
