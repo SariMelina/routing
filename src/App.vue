47c6109de7437e96b2f88cfebb24b1928d0cf0bd
SariMelina <mmkings_beba@hotmail.com>
2021-02-04 14:24:19 -0600

    Importing routes

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
 
 <script>
 import Header from './components/Header.component.vue'
+
 export default {
   components: {
     Header : Header
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Router from 'vue-router'
+import { rutas } from './routes/rutas.js'
 
 Vue.config.productionTip = false
+
+Vue.use(Router)
+ const routing = new Router ({
+   routes: rutas
+ })
 
 new Vue({
   render: h => h(App)
