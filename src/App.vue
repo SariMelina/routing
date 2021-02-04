b69ddc5744ca0681f793cddcff75bca04e2ce72f
SariMelina <mmkings_beba@hotmail.com>
2021-02-04 10:52:10 -0600

    Adding the header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
-
+  <div class="container">
+    <Header/>
+    <div class="jumbotron">
+      
+    </div>
+  </div>
 </template>
+
+<script>
+import Header from './components/Header.component.vue'
+export default {
+  components: {
+    Header : Header
+  }
+}
+</script>
 
 <style>
 
