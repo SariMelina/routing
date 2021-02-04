240df34cb87ed79bb357df52fc826d3133b9f30d
SariMelina <mmkings_beba@hotmail.com>
2021-02-04 14:24:49 -0600

    New components folder created

---
+++ src/components/Home.component.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>HOME</h1>
+        <hr>
+        <img src="https://scipioafricanusblog.files.wordpress.com/2012/11/imagine-dragons.png" alt="">
+    </div>
+</template>
