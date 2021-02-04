e51ec722dc69708cd7d4d85a44469c399b1983b0
SariMelina <mmkings_beba@hotmail.com>
2021-02-04 16:34:40 -0600

    Adding the route for get one user

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -3,5 +3,6 @@ import UsersComponent from '../components/Users.component.vue'
 
 export const rutas = [
     { path: '', component: HomeComponent },
-    { path: '/users', component: UsersComponent }
+    { path: '/users', component: UsersComponent },
+    { path: '/users/:id', component: UsersComponent }
 ]
