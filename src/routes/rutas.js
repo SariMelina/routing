c8419e84c741504fa1117eaec4c07b39b1dc097c
SariMelina <mmkings_beba@hotmail.com>
2021-02-04 17:00:23 -0600

    Adding a new component to the subrout

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -1,9 +1,10 @@
 import HomeComponent from '../components/Home.component.vue'
 import UsersComponent from '../components/Users.component.vue'
+import InfoUser from '../components/InfoUser.component.vue'
 
 export const rutas = [
     { path: '', component: HomeComponent },
     { path: '/users', component: UsersComponent, children: [ 
-        { path: ':id'}
+        { path: ':id', component: InfoUser}
     ] }
 ]
