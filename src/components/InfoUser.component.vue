55eb222be16088744811436a96d8411a9a817bf0
SariMelina <mmkings_beba@hotmail.com>
2021-02-04 17:04:08 -0600

    New component created

---
+++ src/components/InfoUser.component.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h3>Información del Usuario</h3>
+        <div>
+            <p><strong>Nombre:</strong> {{ nombre }}</p>
+            <p><strong>Email:</strong> {{ email }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            nombre : '',
+            email: ''
+        }
+    }, 
+    created () {
+        let id = this.$route.params.id
+        this.$http.get('https://jsonplaceholder.typicode.com/users'+id)
+        .then(resp => {
+            return resp.json()
+        }).then(user => {
+            this.nombre = user.name,
+            this.email = user.email
+        })
+    }
+}
+</script>
